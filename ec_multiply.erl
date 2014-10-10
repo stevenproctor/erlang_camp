7d5c0a8522087a5fd804607222730580549c4ca4
Steven Proctor <steven.proctor@gmail.com>
2014-10-10 10:21:18 -0500

    fix docs on ec_multiply:multi

--- ec_multiply.erl
+++ ec_multiply.erl
@@ -5,7 +5,7 @@
 
 -export([mult/3]).
 
-%% @doc adds two numbers together.
+%% @doc multiplies three numbers together.
 -spec mult(number(), number(), number()) -> number().
 mult(Multiplicand, Multiplier, Multiplier2) ->
     Multiplicand * Multiplier * Multiplier2.
