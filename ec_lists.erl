7e21372b0a6ed76f0dc31c5d317626147a643909
Steven Proctor <steven.proctor@gmail.com>
2014-10-10 12:33:33 -0500

    Lists and higher order functions

--- ec_lists.erl
+++ ec_lists.erl
@@ -1,13 +1,25 @@
 -module(ec_lists).
 
--export([print_each/1]).
+-export([print_each/1,
+         yourmap/2]).
 
 -spec print_each(list()) -> ok.
 print_each(List) ->
     print_each(List, 1).
+
+yourmap(Fun, List) ->
+    yourmap(Fun, List, []).
+
+
 
 print_each([], _) ->
     ok;
 print_each([Head|Rest], Index) ->
     io:format("~p is ~p~n", [Index, Head]),
     print_each(Rest, Index + 1).
+
+
+yourmap(_Fun, [], Accum) ->
+    lists:reverse(Accum);
+yourmap(Fun, [Head|Tail], Accum) ->
+    yourmap(Fun, Tail, [Fun(Head)|Accum]).
